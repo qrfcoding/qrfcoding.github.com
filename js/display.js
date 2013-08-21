1d34a87f25af77b19b9fb6278a415248948c5428
DRcoding <julyellen@163.com>
2013-08-21 17:49:38 +0800

    整体的blog
    基本完善链接

---
+++ js/display.js
@@ -0,0 +1,17 @@
+//左侧伸展按钮
+function hide(){
+	document.getElementById("display").style.display="inline"; //显示开始设置为隐藏的display按钮
+	document.getElementById("hidden").style.display="none";
+	document.getElementById("left").style.display="none"; //隐藏左边
+	document.getElementById("content").style.width="100%"; //改变右边content的width铺满窗口
+	document.getElementById("header").style.width="100%";
+	document.getElementById("pages").style.textAlign="center";
+		}
+ function dis(){
+	document.getElementById("hidden").style.display="inline";
+	 document.getElementById("left").style.display="inline";  //显示左边
+	 document.getElementById("content").style.width="75%";    //改变右边content的width属性
+	 document.getElementById("display").style.display="none"  //隐藏display按钮，使得两次点击看似是同一个按钮
+	 document.getElementById("header").style.width="75%";
+	 document.getElementById("pages").style.textAlign="left";
+	 }
