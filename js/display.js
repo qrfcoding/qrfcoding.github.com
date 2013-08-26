4e43ed25d5681ecd9035dc120d0bc79236a3f497
DRcoding <julyellen@163.com>
2013-08-26 21:50:20 +0800

    RWD
    Responsive

--- js/display.js
+++ js/display.js
@@ -6,6 +6,8 @@ function hide(){
 	document.getElementById("content").style.width="100%"; //改变右边content的width铺满窗口
 	document.getElementById("header").style.width="100%";
 	document.getElementById("pages").style.textAlign="center";
+	 document.getElementById("footer").style.left="18%";
+	
 		}
  function dis(){
 	document.getElementById("hidden").style.display="inline";
@@ -14,4 +16,6 @@ function hide(){
 	 document.getElementById("display").style.display="none"  //隐藏display按钮，使得两次点击看似是同一个按钮
 	 document.getElementById("header").style.width="75%";
 	 document.getElementById("pages").style.textAlign="left";
+	 document.getElementById("footer").style.left="5%";  //为嘛这里是7%了呢？
+	
 	 }
