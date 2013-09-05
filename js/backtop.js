66ddd48a80ed60cdb10194ff7c7b3609438953c2
DRcoding <julyellen@163.com>
2013-09-05 23:56:27 +0800

    code in my eye

---
+++ js/backtop.js
@@ -0,0 +1,25 @@
+﻿ function getScrollTop(){
+        if(window.pageYOffset != null)
+            return window.pageYOffset;
+        var d = window.document;
+        if(document.compatMode == "CSS1Compat")
+            return d.document.documentElement.scrollTop;
+        return d.document.body.scrollTop;
+    }
+    window.onscroll = function(){
+        var top = getScrollTop();
+        var btn = document.getElementById("btn");
+        if(top>0){
+          btn.style.display = "block";
+        }
+        else btn.style.display = "none";
+    }
+    function backTop(){
+         (function() {  
+             var top = getScrollTop();  
+             if (top > 0) {  
+             window.scrollTo(0, top / 1.2)  
+             setTimeout(arguments.callee, 10);  
+             }  
+         })() 
+    }
