ddcf91f429562d5e74f83a3d99a242208ea88563
DRcoding <julyellen@163.com>
2013-09-10 09:23:46 +0800

    width change

--- js/display.js
+++ js/display.js
@@ -12,9 +12,9 @@ function hide(){
  function dis(){
 	document.getElementById("hidden").style.display="inline";
 	 document.getElementById("left").style.display="inline";  //显示左边
-	 document.getElementById("content").style.width="75%";    //改变右边content的width属性
+	 document.getElementById("content").style.width="79%";    //改变右边content的width属性
 	 document.getElementById("display").style.display="none"  //隐藏display按钮，使得两次点击看似是同一个按钮
-	 document.getElementById("header").style.width="75%";
+	 document.getElementById("header").style.width="79%";
 	 document.getElementById("pages").style.textAlign="left";
 	 document.getElementById("footer").style.left="5%";  //为嘛这里是7%了呢？
 	
